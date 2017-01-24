442f681aaca7ee12159de57edb75472165bf7d82
Safiyyah Mitchell <safiyyah.mitchell@bison.howard.edu>
2017-01-24 17:55:15 -0500

    Corrected syntax for returning a null list.

--- fp.rkt
+++ fp.rkt
@@ -1,5 +1,5 @@
 (DEFINE (reverse-general L) ;function definition: name & parameter (takes in a list called L)
-        (if(NULL? L) ())    ;condition for empty list; if list is empty, return "()"
+        (if(NULL? L) '())    ;condition for empty list; if list is empty, return "()"
 
         
 )
