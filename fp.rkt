bd8bf5b3a58b074e10e233c0263d0a7e80634c83
Safiyyah Mitchell <safiyyah.mitchell@bison.howard.edu>
2017-01-27 07:47:32 -0500

    More functionality added in conditional

--- fp.rkt
+++ fp.rkt
@@ -7,7 +7,6 @@
         ) ;closes out conditional
 ) ;closes out funtion definition
 
-(reverse-general '(a b (b f)))
 
 (DEFINE (sum-up-numbers-simple L) ;function definition: name and parameter (takes in a list called L)
         (COND
@@ -16,9 +15,8 @@
          ((NOT (number? (car L))) (sum-up-numbers-simple (cdr L))) ;if first element is not a number, disregard and continue looking for numbers
          (ELSE (+ (car L) (sum-up-numbers-simple (cdr L)))) ; else - performs addition on numbers in list, recursively
          ) ;closes out conditional 
-)
-         
-(sum-up-numbers-simple '(a 100 300 (100)))
+) ;closes out function
+        
 
 (DEFINE (sum-up-numbers-general L) ;function definition: name and parameter (takes in a list called L)
         (COND
@@ -26,24 +24,24 @@
          ((NOT (number? (car L))) (sum-up-numbers-general (cdr L))) ;if first element is not a list, disregard and continue looking for numbers
          ((LIST? L) ;checks if L is a list
           (+ (car L) (sum-up-numbers-general (cdr L)))) ;performs summation of L
-          ((LIST? L) ; *should check if there is a list within the list
+          ((LIST? L) ;*should check if there is a list within the list
            (car L) (+ (sum-up-numbers-general (car L)) (sum-up-numbers-general(cdr L)))) ;adds numbers in nested list to sum of list
-        )
-)
+        ) ;closes out conditional
+) ;closes out function
 
 (sum-up-numbers-general '(100 a 200 (200)))
 
-(DEFINE (mini L)
+(DEFINE (mini L) ;helper function to get the minimum value in a list
         (COND
-         ((NOT (number? (car L))) (mini (cdr L)))
-         ((NULL? (cdr L)) (car L))
-         ((< (car L) (mini (cdr L))) (car L))
+         ((NULL? L) '()) ;checks for an empty list - if empty, return empty parentheses
+         ((NOT (number? (car L))) (mini (cdr L))) ;checks for any non numerical values
+         ((NULL? (cdr L)) (car L)) ;condition checks for 1 value in list; if there is only one value, it returns said value
+         ((< (car L) (mini (cdr L))) (car L)) ;compares values to find minimum
          (ELSE (mini (cdr L)))
-         )
+         ) ;closes out conditional
+) ;closes out function
+
+(DEFINE (min-above-min LI L2) ;function def: name and parameter (takes in two lists called L1 and L2)
+
 )
-
-(mini '(a b 100 300))
-;(DEFINE (min-above-min LI L2) ;function def: name and parameter (takes in two lists called L1 and L2)
-
-;)
 
